fe67f0d0ef46cd705587902af5790aa3391ace34
ShevArtV <shev.art.v@ya.ru>
2022-07-16 00:21:16 +0300

    Order methods are begining

--- public_html/assets/components/minishop2/js/web/custom.js
+++ public_html/assets/components/minishop2/js/web/custom.js
@@ -3,6 +3,10 @@ class MiniShop {
         this.config = {
             'triggerElementSelector': '[data-ms-action]',
             'cartBlockSelector': '[data-ms-cart]',
+            'orderBlockSelector': '[data-ms-order]',
+            'deliveryFieldSelector': '[name="delivery"]',
+            'paymentFieldSelector': '[name="payment"]',
+            'anotherFieldSelector': '[data-ms-field]',
             'actionUrl': '/assets/components/minishop2/action.php',
             'hideClass': 'd-none',
             'selectorPrefix': 'ms-'
@@ -13,13 +17,17 @@ class MiniShop {
                 before_send: 'ms_before_send',
             },
         };
+
         this.config = Object.assign(this.config, config);
         this.initialize();
     }
 
     initialize() {
         if (!this.cart) {
-            this.cart = new Cart(this.config);
+            this.cart = new msCart(this.config);
+        }
+        if (!this.order) {
+            this.order = new msOrder(this.config);
         }
         this.addListener(this.config.triggerElementSelector);
     }
@@ -29,39 +37,164 @@ class MiniShop {
         headers = headers ? headers : {"X-Requested-With": "XMLHttpRequest"};
         url = url ? url : this.config.actionUrl;
 
-        let data = {instance: this, params: params};
+        let data = {
+            instance: this,
+            params: params,
+            callbacks: {
+                errorHandlers: {
+                    before: {},
+                    after: {}
+                },
+                successHandlers: {
+                    before: {},
+                    after: {}
+                }
+            }
+        };
         if (!this.fire(this.events.ms.before_send, data)) {
+            if (data.callbacks.errorHandlers.before.length) {
+                this.runCallbacks(data.callbacks.errorHandlers.before, data);
+            }
             this.errorHandler(data);
             return false;
         }
+        if (data.callbacks.successHandlers.before.length) {
+            this.runCallbacks(data.callbacks.successHandlers.before, data);
+        }
 
         let options = {
-                method: method,
-                headers: headers,
-                body: params
-            };
+            method: method,
+            headers: headers,
+            body: data.params
+        };
 
         fetch(url, options)
             .then(response => response.json())
-            .then(result => this.responseHandler(params, result));
+            .then(result => this.responseHandler(result, data));
     }
 
     prepare(elem) {
         return new FormData(elem.closest('form'));
     }
 
-    responseHandler(params, response) {
-        let action = params.get('ms2_action'),
-            data = {instance: this, params: params, response: response, action: action};
-        if (!this.fire(this.events.ms.after_send, data) || !response.success) {
-            this.errorHandler(data);
-        } else {
-            this.successHandler(data);
+    responseHandler(response, data) {
+        if (data.params.get('ms_action')) {
+            data.action = data.params.get('ms_action');
+            data.response = response;
+            if (!this.fire(this.events.ms.after_send, data) || !response.success) {
+                if (data.callbacks.errorHandlers.after.length) {
+                    this.runCallbacks(data.callbacks.errorHandlers.after, data);
+                }
+                if (data.instance.errorHandler) {
+                    data.instance.errorHandler(data);
+                } else {
+                    this.errorHandler(data);
+                }
+            } else {
+                if (data.callbacks.successHandlers.after.length) {
+                    this.runCallbacks(data.callbacks.successHandlers.after, data);
+                }
+                if (data.instance.successHandler) {
+                    data.instance.successHandler(data);
+                } else {
+                    this.successHandler(data);
+                }
+            }
         }
     }
 
     errorHandler(data) {
         console.log('Ошибка', data);
+    }
+
+    successHandler(data) {
+        console.log('Успех', data);
+    }
+
+    fire(eventName, data) {
+        data = data || {};
+        let event = new CustomEvent(eventName, {bubbles: true, cancelable: true, composed: true, detail: data});
+        if (!document.dispatchEvent(event)) {
+            return false;
+        }
+        return true;
+    }
+
+    addListener(selector, scope, className, methodName) {
+        scope = scope || document;
+        let elements = scope.querySelectorAll(selector);
+        if (elements) {
+            elements.forEach(el => {
+                let eventName = 'click';
+                if (el.dataset.msAction) {
+                    let msAction = el.dataset.msAction.split('/');
+                    className = msAction[0];
+                    methodName = msAction[1];
+                }
+
+                if (['INPUT', 'TEXTAREA', 'SELECT'].indexOf(el.tagName) !== -1) {
+                    eventName = 'change';
+                }
+
+                el.addEventListener(eventName, e => {
+                    e.preventDefault();
+                    this[className][methodName](this.prepare(el), el);
+                });
+            });
+        }
+    }
+
+    runCallbacks(callbacks, data) {
+        for (let f in callbacks) {
+            callbacks[f](data);
+        }
+    }
+
+}
+
+class msCart extends MiniShop {
+    initialize() {
+        this.cart = this;
+    }
+
+    add(params, el) {
+        // api/cart/product - POST
+        params.append('ms_action', 'cart/add');
+        params.append('ms2_action', 'cart/add');
+        super.send(params);
+    }
+
+    change(params, el) {
+        // api/cart/product - PUT
+        params.append('ms_action', 'cart/change');
+        params.append('ms2_action', 'cart/change');
+        super.send(params);
+
+    }
+
+    remove(params, el) {
+        // api/cart/product - DELETE
+        params.append('ms_action', 'cart/remove');
+        params.append('ms2_action', 'cart/remove');
+        super.send(params);
+    }
+
+    clean(params, el) {
+        // api/cart/products - DELETE
+        params.append('ms_action', 'cart/clean');
+        params.append('ms2_action', 'cart/clean');
+        super.send(params);
+    }
+
+    status(params) {
+        // api/cart/products - GET
+        params.append('ms_action', 'cart/status');
+        params.append('ms2_action', 'cart/status');
+        super.send(params);
+    }
+
+    removeItem(key) {
+        document.getElementById(key).remove();
     }
 
     successHandler(data) {
@@ -79,98 +212,118 @@ class MiniShop {
                     self.addListener(self.config.triggerElementSelector, target);
                 }
             }
-
         }
-        console.log('Успех', data);
+    }
+}
+
+class msOrder extends MiniShop {
+    initialize() {
+        this.params = new FormData();
+        this.order = this;
+        this.orderBlock = document.querySelector(this.config.orderBlockSelector);
+        if (this.orderBlock) {
+            //this.updateFields(this.params);
+            this.addListener(this.config.anotherFieldSelector, this.orderBlock, 'order', 'add');
+        }
+        // TODO понять почему синхронно в заказе не меняются доставка и оплата
+        this.add(this.params, this.orderBlock.querySelector(this.config.deliveryFieldSelector + ':checked'));
+        this.updatePayments(this.params, this.orderBlock.querySelector(this.config.deliveryFieldSelector + ':checked'));
+        this.addListener(this.config.deliveryFieldSelector, this.orderBlock, 'order', 'updatePayments');
     }
 
-    fire(eventName, data) {
-        data = data || {};
-        let event = new CustomEvent(eventName, {bubbles: true, cancelable: true, composed: false, detail: data});
-        if (!document.dispatchEvent(event)) {
-            this.errorHandler(data);
-            return false;
-        }
-        return true;
-    }
-
-    addListener(selector, scope) {
-        scope = scope || document;
-        let elements = scope.querySelectorAll(selector);
-        if (elements) {
-            elements.forEach(el => {
-                let msAction = el.dataset.msAction.split('/'),
-                    className = msAction[0],
-                    methodName = msAction[1],
-                    eventName = 'click';
-                if (['INPUT', 'TEXTAREA', 'SELECT'].indexOf(el.tagName) !== -1) {
-                    eventName = 'change';
+    updateFields(params) {
+        let allFields = this.orderBlock.querySelectorAll(this.config.anotherFieldSelector);
+        if (allFields.length) {
+            allFields.forEach(el => {
+                if (el.value && el.tagName !== 'BUTTON') {
+                    switch (el.type) {
+                        case 'checkbox':
+                        case 'radio':
+                            if (el.checked) {
+                                this.add(this.params, el);
+                            }
+                            break;
+                        default:
+                            this.add(this.params, el);
+                            break;
+                    }
                 }
-
-                el.addEventListener(eventName, e => {
-                    e.preventDefault();
-                    this[className][methodName](this.prepare(el));
-                });
             });
         }
+        this.getrequired(params);
     }
 
+    add(params, el) {
+        // api/order/add - GET
+        console.log(el.name, el.value);
+        if(el.name && el.value){
+            params.append('ms_action', 'order/add');
+            params.append('ms2_action', 'order/add');
+            params.append('key', el.name);
+            params.append('value', el.value);
+            super.send(params);
+        }
+    }
+
+    getrequired(params) {
+        // api/order/required - GET
+        params.append('ms_action', 'order/getrequired');
+        params.append('ms2_action', 'order/getrequired');
+        super.send(params);
+    }
+
+    updatePayments(params, el) {
+        if (el.checked) {
+            let paymentFields = this.orderBlock.querySelectorAll(this.config.paymentFieldSelector),
+                paymentAllow = el.dataset.payments;
+            if (paymentFields) {
+                paymentFields.forEach(el => {
+                    paymentAllow.indexOf(el.value) === -1 ? el.disabled = true : el.disabled = false;
+                });
+                let curPayment = this.orderBlock.querySelector(this.config.paymentFieldSelector + ':checked');
+                if(curPayment.disabled){
+                    curPayment.checked = false;
+                    paymentFields.forEach(el => {
+                        if(el.disabled === false){
+                            el.checked = true;
+                            this.add(params,el);
+                            return true;
+                        }
+                    });
+                }
+            }
+            this.add(params, el);
+        }
+    }
+
+    getcost() {
+    }
+
+    clean() {
+    }
+
+    submit() {
+    }
 }
 
-class Cart extends MiniShop {
-    initialize() {
-        this.cart = this;
-    }
 
-    add(params) {
-        // api/cart/product - POST
-        params.append('ms2_action', 'cart/add');
-        super.send(params);
+document.addEventListener('ms_after_send', e => {
+    if (e.detail.action == 'cart/add') {
+        //blabla
     }
+});
 
-    change(params) {
-        // api/cart/product - PUT
-        params.append('ms2_action', 'cart/change');
-        super.send(params);
-
+document.addEventListener('ms_before_send', e => {
+    e.detail.callbacks.errorHandlers.before['testhandler'] = function (data) {
+        data.test = 'bla-bla';
     }
-
-    remove(params) {
-        // api/cart/product - DELETE
-        params.append('ms2_action', 'cart/remove');
-        super.send(params);
+    e.detail.callbacks.errorHandlers.before['testhandler2'] = function (data) {
+        data.test = 'bla-bla-bla';
     }
+    e.detail.params.append('test', 1);
 
-    clean(params) {
-        // api/cart/products - DELETE
-        params.append('ms2_action', 'cart/clean');
-        super.send(params);
-    }
-
-    status(params) {
-        // api/cart/products - GET
-        params.append('ms2_action', 'cart/status');
-        super.send(params);
-    }
-
-    removeItem(key) {
-        document.getElementById(key).remove();
-    }
-}
+});
 
 const minishop = new MiniShop({});
 
-if (minishop) {
-    document.addEventListener('ms_after_send', e => {
-        if(e.detail.action == 'cart/add'){
-            //blabla
-        }
-    });
-    document.addEventListener('ms_after_cart_add', e => {
-        //blabla
-    });
-    document.addEventListener('ms_before_send', e => {
-        console.log(e.detail);
-        e.detail.params.append('test', 1);
-    });
-}
+
--- public_html/core/components/minishop2/elements/chunks/ms3/chunk.ms_cart_wrap.tpl
+++ public_html/core/components/minishop2/elements/chunks/ms3/chunk.ms_cart_wrap.tpl
@@ -1,38 +1,42 @@
 {if $products}
-<div class="table-responsive">
-    <table class="table table-striped">
-        <tr class="header">
-            <th class="title">{'ms2_cart_title' | lexicon}</th>
-            <th class="count">{'ms2_cart_count' | lexicon}</th>
-            <th class="weight">{'ms2_cart_weight' | lexicon}</th>
-            <th class="price">{'ms2_cart_price' | lexicon}</th>
-            <th class="cost">{'ms2_cart_cost' | lexicon}</th>
-            <th class="remove"></th>
-        </tr>
-        {$products}
-        <tr class="footer">
-            <th class="total">{'ms2_cart_total' | lexicon}:</th>
-            <th class="total_count">
-                <span class="ms2_total_count">{$status['total_count']}</span>
-                {'ms2_frontend_count_unit' | lexicon}
-            </th>
-            <th class="total_weight text-nowrap" colspan="2">
-                <span class="ms2_total_weight">{$status['total_weight']}</span>
-                {'ms2_frontend_weight_unit' | lexicon}
-            </th>
-            <th class="total_cost text-nowrap" colspan="2">
-                <span class="ms2_total_cost">{$status['total_cost']}</span>
-                {'ms2_frontend_currency' | lexicon}
-            </th>
-        </tr>
-    </table>
-</div>
-
-<form method="post" class="ms2_form">
-    <button type="submit" name="ms2_action" value="cart/clean" class="btn btn-danger">
-        {'ms2_cart_clean' | lexicon}
-    </button>
-</form>
+    <div class="table-responsive">
+        <table class="table table-striped">
+            <tr class="header">
+                <th class="title">{'ms2_cart_title' | lexicon}</th>
+                <th class="count">{'ms2_cart_count' | lexicon}</th>
+                <th class="weight">{'ms2_cart_weight' | lexicon}</th>
+                <th class="price">{'ms2_cart_price' | lexicon}</th>
+                <th class="cost">{'ms2_cart_cost' | lexicon}</th>
+                <th class="remove"></th>
+            </tr>
+            {$products}
+            <tr class="footer">
+                <th class="total">{'ms2_cart_total' | lexicon}:</th>
+                <th class="total_count">
+                    <span class="ms2_total_count">{$status['total_count']}</span>
+                    {'ms2_frontend_count_unit' | lexicon}
+                </th>
+                <th class="total_weight text-nowrap" colspan="2">
+                    <span class="ms2_total_weight">{$status['total_weight']}</span>
+                    {'ms2_frontend_weight_unit' | lexicon}
+                </th>
+                <th class="total_cost text-nowrap" colspan="2">
+                    <span class="ms2_total_cost">{$status['total_cost']}</span>
+                    {'ms2_frontend_currency' | lexicon}
+                </th>
+            </tr>
+        </table>
+    </div>
+    <div class="d-flex justify-content-between">
+       {* <a href="{6 | url}" class="btn btn-success">
+            Оформить заказ
+        </a>*}
+        <form method="post" class="ms2_form">
+            <button type="submit" name="ms2_action" value="cart/clean" class="btn btn-danger" data-ms-action="cart/clean">
+                {'ms2_cart_clean' | lexicon}
+            </button>
+        </form>
+    </div>
 {else}
     <div class="alert alert-warning" {!($products | length == 0) ? 'class="d-none"':''}>
         {'ms2_cart_is_empty' | lexicon}
